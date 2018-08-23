4dbd9d802127a6076602e58576f989bf5dacd91c
yutaf <yutafuji2008@gmail.com>
2018-08-23 14:26:35 +0900

    Add up navigation

--- app/src/main/java/com/example/yuta/ringflashwithoutuisample/WaitActivity.java
+++ app/src/main/java/com/example/yuta/ringflashwithoutuisample/WaitActivity.java
@@ -1,7 +1,6 @@
 package com.example.yuta.ringflashwithoutuisample;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
